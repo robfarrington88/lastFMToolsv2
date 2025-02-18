731bfb2cf36dbd6d3441e4284d810f6b514292d8
robfarrington88 <robjamesfarrington@googlemail.com>
2025-02-18 20:33:42 +0000

    updated runscript

--- runscript.py
+++ runscript.py
@@ -1,3 +1,8 @@
 import LastLib2 as ll
+import time
 
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 ll.createDatabase()
+for i in range(2010,2026):
+    ll.getAnnualScrobblesToDB(i)
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
